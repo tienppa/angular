1c88bf66a96e46b4b3e8b582ff391ce6a67084cb
Tien Dat <tienppa@gmail.com>
2022-06-26 23:20:38 +0700

    ng bootstrap

--- medix/src/app/pages/doctors/doctor-list/doctor-list.component.ts
+++ medix/src/app/pages/doctors/doctor-list/doctor-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-doctor-list',
   templateUrl: './doctor-list.component.html',
-  styleUrls: ['./doctor-list.component.scss']
+  styleUrls: ['./doctor-list.component.scss'],
 })
 export class DoctorListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
+  ngOnInit() {}
+
+  open(content: any) {
+    this.modalService.open(content);
+  }
 }
--- medix/src/app/pages/doctors/doctor-modify/doctor-modify.component.ts
+++ medix/src/app/pages/doctors/doctor-modify/doctor-modify.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-doctor-modify',
   templateUrl: './doctor-modify.component.html',
-  styleUrls: ['./doctor-modify.component.scss']
+  providers: [NgbModalConfig, NgbModal],
 })
 export class DoctorModifyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
+  open(content: any) {
+    this.modalService.open(content);
+  }
+
+  ngOnInit() {}
 }
